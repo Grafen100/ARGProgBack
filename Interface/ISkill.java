3e867e6c71772d550ec230952cd1a739b3fcac33
Hannover 1917 <99442821+Grafen100@users.noreply.github.com>
2023-06-22 18:58:49 -0300

    Update ISkill.java
    edit of description

--- Interface/ISkill.java
+++ Interface/ISkill.java
@@ -9,9 +9,18 @@ import java.util.List;
  */
 public interface ISkill {
     
+    //Crear una habilidad
     public Skill createSkill (Skill skill);
+    
+    //Eliminar una habilidad buscada por ID
     public void deleteSkill (int id);
+    
+    //Guardar una habilidad
     public void saveSkill (Skill skill);
+    
+    //Buscar una habilidad por ID
     public Skill findSkill (int id);
+    
+    // Traer una lista de habilidades
     public List<Skill> getSkill();
 }
