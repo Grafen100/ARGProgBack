faf71feb1c845411b5a1f514bfc1b86a34cda6d7
Hannover 1917 <99442821+Grafen100@users.noreply.github.com>
2023-07-01 17:59:04 -0300

    Update RPeron.java
    change

--- Repository/RPerson.java
+++ Repository/RPerson.java
@@ -1,6 +1,7 @@
 package com.portfolio.amo.Repository;
 
 import com.portfolio.amo.Entity.Person;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +13,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface RPerson extends JpaRepository <Person,Integer>{
 
+    public Optional<Person> findByNombre(String nombre);
+    public boolean existsByNombre(String nombre);
 }
