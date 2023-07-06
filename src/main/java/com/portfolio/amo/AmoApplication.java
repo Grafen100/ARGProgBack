2b337d0a3001a82b6bf3f68c576ad4d8a7b247a1
Hannover 1917 <99442821+Grafen100@users.noreply.github.com>
2023-07-05 22:11:32 -0300

    delete Application and WebConfig
    delete out file

--- src/main/java/com/portfolio/amo/AmoApplication.java
+++ src/main/java/com/portfolio/amo/AmoApplication.java
@@ -3,6 +3,7 @@ package com.portfolio.amo;
 import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -32,7 +33,7 @@ public class AmoApplication {
 			}
 		};
 	}
-	
+	@Bean
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
